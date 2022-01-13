05088641cd603a36026db259d9443a0b94fabac4
Stephen Lee <stephenabcba@gmail.com>
2022-01-12 17:39:27 -0800

    updated to include error message for wrong path

--- flask/understanding_routing/server.py
+++ flask/understanding_routing/server.py
@@ -9,13 +9,17 @@ def hello_world():
 def dojo():
     return("Dojo!")
 
-@app.route('/say/<word>')
+@app.route('/say/<string:word>')
 def say(word):
     return(f"Hi {word.title()}!")
 
 @app.route('/repeat/<int:times>/<string:word>')
 def repeat(times, word):
     return (word * times)
+
+@app.route('/<path:anything>')
+def sorry(anything):
+    return("Sorry! No response. Try again.")
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.

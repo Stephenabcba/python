5c11d6bf9a1a5810f1ce762643e087154dc329b8
Stephen Lee <stephenabcba@gmail.com>
2022-01-14 15:54:56 -0800

    updated for leaderboard functionality

--- flask/fundamentals/great_number_game/server.py
+++ flask/fundamentals/great_number_game/server.py
@@ -11,6 +11,7 @@ def hello_world():
     if "target_number" not in session:
         session["target_number"] = randint(1,100)
         session["result"] = None
+        session["guess_count"] = 0
     return render_template("index.html")
 
 @app.route('/guess', methods=["POST"])
@@ -25,12 +26,34 @@ def guess():
             session["result"] = "Low"
         else:
             session["result"] = "correct"
+        session["guess_count"] += 1
     else:
         session["result"] = "invalid"
     return redirect("/")
 
 @app.route('/restart', methods=["POST"])
 def restart():
+    # session.clear()
+    session.pop("target_number")
+    session.pop("result")
+    session.pop("guess_count")
+    return redirect("/")
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    if "leaderboard" not in session:
+        session["leaderboard"] = []
+    session["leaderboard"] += [[request.form["name"], session["guess_count"]]]
+    return redirect("/leaderboard")
+
+
+@app.route('/leaderboard') 
+def leaderboard():
+    session["leaderboard"] = sorted(session["leaderboard"],key=lambda x:x[1])
+    return render_template("leaderboard.html")
+
+@app.route('/clear_session') 
+def clear_session():
     session.clear()
     return redirect("/")
 

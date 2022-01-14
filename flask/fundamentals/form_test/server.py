34d2afab91591421ec28d1af57935b7b28a675c9
Stephen Lee <stephenabcba@gmail.com>
2022-01-14 10:44:31 -0800

    added functionality to forms to not render in post

--- flask/fundamentals/form_test/server.py
+++ flask/fundamentals/form_test/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template,  request, redirect
+from flask import Flask, render_template,  request, redirect, session
 app = Flask(__name__)
+app.secret_key = "keep it secret, keep it safe"
 # our index route will handle rendering our form
 @app.route('/')
 def index():
@@ -8,10 +9,16 @@ def index():
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
+    # print(request.form)
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
     # Never render a template on a POST request.
     # Instead we will redirect to our index route.
-    return redirect('/')
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
 
 
 

e12fbad09e5b61442a45ccd49a146a1cd4febfd9
Stephen Lee <stephenabcba@gmail.com>
2022-01-24 15:05:34 -0800

    testing css with tailwind

---
+++ flask_mysql/crud/users_crud_modularized_with_tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./flask_app/templates/create.html","./flask_app/templates/edit.html","./flask_app/templates/index.html","./flask_app/templates/show.html"],
+  theme: {
+    extend: {},
+    colors: {
+        'background': '#3c454f',
+        'foreground-text' : '#ffde0a',
+        'foreground-highlight' : '#3b4d61',
+        'foreground-button' : '#f9a8d4',
+        'foreground-button-text' : 'white',
+    }
+  },
+  plugins: [],
+}
